f30fa68d72dd11e86156848e73ae0a2901ba7352
samirbhandari-123 <samirbhandari690@gmail.com>
2025-11-12 11:59:21 +0545

    Task 1 completed

---
+++ Task 1/Operators.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Operators
+    {
+        // Method to add two numbers
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("Addition: " + (a + b));
+        }
+
+        // Method to subtract two numbers
+        public void Subtract(int a, int b)
+        {
+            Console.WriteLine("Subtraction: " + (a - b));
+        }
+
+        // Method to multiply two numbers
+        public void Multiply(int a, int b)
+        {
+            Console.WriteLine("Multiplication: " + (a * b));
+        }
+
+        // Method to divide two numbers
+        public void Divide(int a, int b)
+        {
+            if (b != 0)
+                Console.WriteLine("Division: " + ((double)a / b));
+            else
+                Console.WriteLine("Cannot divide by zero!");
+        }
+
+        // Method to check if a number is odd or even using ternary operator
+        public void OddEvenFinder(int number)
+        {
+            string result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+            Console.WriteLine(result);
+        }
+    }
+}
--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Task_1
+﻿using System;
+
+namespace Task_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create object of Operators class
+            Operators op = new Operators();
+
+            // Take user input for operations
+            Console.Write("Enter first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n--- Results ---");
+
+            // Call all methods of Operators class
+            op.Add(a, b);
+            op.Subtract(a, b);
+            op.Multiply(a, b);
+            op.Divide(a, b);
+
+            // Call OddEvenFinder method
+            Console.Write("\nEnter a number to check Odd or Even: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            op.OddEvenFinder(number);
+
+            Console.ReadLine();
         }
     }
 }
