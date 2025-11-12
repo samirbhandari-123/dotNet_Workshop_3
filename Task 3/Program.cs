816fa4850eea22ec3374c8ea35d9bf588907c878
samirbhandari-123 <samirbhandari690@gmail.com>
2025-11-12 19:34:58 +0545

    Task 4 completed

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -24,7 +24,7 @@ namespace Task_3
                 Console.WriteLine("You are an adult.");
             }
 
-            Console.ReadLine(); // Keep console open
+            Console.ReadLine(); 
         }
     }
 }
--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Task_4
+﻿using System;
+
+namespace Task_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Ask user to input a number between 1 and 7
+            Console.Write("Enter a number (1-7): ");
+            int dayNumber = int.Parse(Console.ReadLine());
+
+            // Using switch statement to print the day of the week
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day number.");
+                    break;
+            }
+
+            Console.ReadLine(); // Keep console open
         }
     }
 }
